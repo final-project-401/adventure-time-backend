5fbdcddf48be07a5274b46e7c5c17c462235662f
Donna Ada <125130284+donnaada@users.noreply.github.com>
2023-07-26 02:34:45 +1000

    changed events.date to sting, added delete, and started removing hardcode from email.js

--- src/models/events.js
+++ src/models/events.js
@@ -1,7 +1,7 @@
 const eventModel = (sequelize, DataTypes) => sequelize.define('events', {
   name: { type: DataTypes.STRING },
   desc: { type: DataTypes.STRING },
-  date: { type: DataTypes.INTEGER },
+  date: { type: DataTypes.STRING },
   travelBuddies: { type: DataTypes.STRING },
   time: { type: DataTypes.STRING },
   userId: { type: DataTypes.INTEGER, allowNull: false },
--- src/modules/email/index.js
+++ src/modules/email/index.js
@@ -6,17 +6,17 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 function sendEmail(req, res, next) {
 
   const msg = {
-    to: 'soldbykenya@gmail.com',
+    to: 'donnaada@icloud.com',
     from: 'itsjoshcoffey@gmail.com',
     subject: 'You have been invited to an event',
-    text: ' ',
+    text: req.query.text,
     html: '<strong>There has been an update to the schedule</strong>',
   };
 
   sgMail
     .send(msg)
     .then(() => {
-      console.log('Email sent');
+      console.log('Email sent', msg);
     })
     .catch((error) => {
       console.error(error);
