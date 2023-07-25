de6b5add9fa689c6f797c30620995199f3504c69
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-07-25 17:00:22 -0500

    auth and emails

--- src/models/events.js
+++ src/models/events.js
@@ -1,9 +1,9 @@
 const eventModel = (sequelize, DataTypes) => sequelize.define('events', {
   name: { type: DataTypes.STRING },
   desc: { type: DataTypes.STRING },
-  date: { type: DataTypes.STRING },
-  travelBuddies: { type: DataTypes.STRING },
-  time: { type: DataTypes.STRING },
+  date: { type: DataTypes.DATE },
+  travelBuddies: { type: DataTypes.ARRAY(DataTypes.STRING) },
+  time: { type: DataTypes.TIME },
   userId: { type: DataTypes.INTEGER, allowNull: false },
 
 });
